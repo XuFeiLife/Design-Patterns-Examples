6b496a1e64d3b2cb7c756939332fcef1f065d64b
XuFeiLife <fei.xu.life@outlook.com>
2023-08-24 16:40:22 +0800

    no message

---
+++ abstractfactory/AndroidOperationController.java
@@ -0,0 +1,7 @@
+// 4. 具体产品类
+class AndroidOperationController implements OperationController {
+    @Override
+    public void control() {
+        System.out.println("Android Operation Controller");
+    }
+}
